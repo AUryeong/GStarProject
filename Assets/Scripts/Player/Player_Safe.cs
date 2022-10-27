645205e5cdf17aae666b9f67a2cc74248c54ceb1
AUryeong <jykee7030@gmail.com>
2022-10-27 20:47:18 +0900

    fix magnet system

--- Assets/Scripts/Player/Player_Safe.cs
+++ Assets/Scripts/Player/Player_Safe.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Player_Safe : Player
 {
+    bool magnetActive;
 
     protected override void OnEnable()
     {
         base.OnEnable();
         hpRemoveValue = 0.5f;
+        magnetActive = false;
     }
 
     protected override void LiveUpdate(float deltaTime)
     {
-        if (hp / fHp >= 0.5f)
+        if (hp / fHp >= 0.5f && !magnetActive)
+        {
+            magnetActive = true;
             magnetSize = 3;
-        else
+        }
+        else if (magnetActive)
+        {
+            magnetActive = false;
             magnetSize = 0;
+        }
     }
 }
