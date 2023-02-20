d2b4f474c5ce524f317d0c5bfb4cfdf652f01cd4
AUryeong <jykee7030@gmail.coma>
2023-02-21 05:44:36 +0900

    fix can set ingredient's negative

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -8,35 +8,40 @@ public class Ingredient : MonoBehaviour
 {
     protected SpriteRenderer spriteRenderer;
 
-    public int ingredientIdx = 0;
+    public int ingredientIdx { get; protected set; }
+
+    [SerializeField] protected bool isNegative = true;
     private readonly int negative = (int)Ingredients.Type.Kimchi;
+
     protected virtual void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     protected virtual void OnEnable()
     {
-        ingredientIdx = Random.Range(0, GameManager.Instance.Inside.Stats.Count);
+        ingredientIdx = isNegative ? Random.Range(negative, GameManager.Instance.Inside.Stats.Count) : Random.Range(0, negative);
         gameObject.layer = IsPositive() ? LayerMask.NameToLayer("Getable") : LayerMask.NameToLayer("Default");
         spriteRenderer.sprite = GameManager.Instance.Inside.Stats[ingredientIdx].OutlineSprite;
     }
+
     public virtual void OnGet()
     {
-        if (IsPositive())
+        if (isNegative)
+        {
+            QusetManager.Instance.QusetUpdate(QuestType.Aweek, 5, 1);
+            QusetManager.Instance.QusetUpdate(QuestType.Main, 6, 1);
+        }
+        else
         {
             QusetManager.Instance.QusetUpdate(QuestType.Day, 4, 1);
             QusetManager.Instance.QusetUpdate(QuestType.Aweek, 4, 1);
             QusetManager.Instance.QusetUpdate(QuestType.Main, 5, 1);
         }
-        else
-        {
-            QusetManager.Instance.QusetUpdate(QuestType.Aweek, 5, 1);
-            QusetManager.Instance.QusetUpdate(QuestType.Main, 6, 1);
-        }
     }
 
     public bool IsPositive()
     {
-        return ingredientIdx < negative;
+        return !isNegative;
     }
 }
